2ffd297f5b4d3a61880af29c72c0552b6b15d420
evlx <evilnyagaming@yandex.ru>
2024-12-19 12:40:24 +0300

    создан базовый шаблон и страница реестра приказов

--- orders/views.py
+++ orders/views.py
@@ -3,4 +3,4 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = 'contracts/index.html'
+    template_name = 'orders/index.html'

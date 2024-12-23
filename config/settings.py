0cd887c277dbfb2d6a0b4b8c626f848bf9d061ea
evlx <evilnyagaming@yandex.ru>
2024-12-23 13:36:52 +0300

    исправлено отображение полей при добавлении и редактировании приказа

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'orders:index'
--- orders/models.py
+++ orders/models.py
@@ -1,25 +1,36 @@
+from datetime import datetime
+from django.utils import timezone
 from django.db import models
 import os
-from django.utils import timezone
+
 
 # Create your models here.
 def order_scan_upload_to(instance, filename):
-    # Генерируем имя файла в формате год_месяц_день_номерПриказа
-    order_date = timezone.localdate(instance.issue_date)
-    year = order_date.year
-    month = f"{order_date.month:02}"  # Делаем месяц двузначным
-    day = f"{order_date.day:02}"  # Делаем день двузначным
+    # Преобразуем дату в объект datetime
+    order_datetime = datetime.combine(instance.issue_date, datetime.min.time())
+    aware_datetime = timezone.make_aware(order_datetime)
+    # Локализуем объект datetime в текущей временной зоне
+    localized_datetime = timezone.localtime(aware_datetime)
 
-    # Формируем путь для хранения файла
-    filename = f"{year}_{month}_{day}_{instance.document_number}.pdf"  # предположим, что файл PDF
-    return os.path.join('orders_scan', str(year), filename)  # Задаем директорию по годам
+    # Теперь можем безопасно получать год, месяц и день
+    year = localized_datetime.year
+    month = f"{localized_datetime.month:02}"
+    day = f"{localized_datetime.day:02}"
+
+    # Формируем имя файла
+    filename = f"{instance.document_number}_{day}.{month}.{year}.pdf"
+
+    # Возвращаем полный путь для сохранения файла
+    return os.path.join('orders_scan', str(year), filename)
+
 
 class Order(models.Model):
     document_number = models.CharField(
         max_length=50,
         verbose_name='Номер документа',
         db_index=True)
-    issue_date = models.DateField(verbose_name='Дата издания')
+    issue_date = models.DateField(
+        verbose_name='Дата издания')
     document_title = models.CharField(
         max_length=255,
         verbose_name='Название документа',
@@ -28,18 +39,31 @@ class Order(models.Model):
         max_length=150,
         verbose_name='Кем подписан документ')
     responsible_executor = models.CharField(
-        max_length=150, verbose_name='Ответственный исполнитель')
+        max_length=150,
+        verbose_name='Ответственный исполнитель')
     transferred_to_execution = models.CharField(
         max_length=150,
-        verbose_name='Кому передан (ответственный за исполнение приказа)')
+        verbose_name='Кому передан (ответственный за исполнение приказа)',
+        blank=True)
     transferred_for_storage = models.CharField(
-        max_length=150, verbose_name='Кому передано на хранение')
+        max_length=150,
+        verbose_name='Кому передано на хранение',
+        blank=True)
     heraldic_blank_number = models.CharField(
-        max_length=20, verbose_name='Номер гербового бланка', blank=True)
-    note = models.TextField(verbose_name='Примечание', blank=True)
-    is_active = models.BooleanField(default=True, verbose_name='Действующий')
+        max_length=20,
+        verbose_name='Номер гербового бланка')
+    note = models.TextField(
+        verbose_name='Примечание',
+        blank=True)
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='Действующий')
 
-    scan = models.FileField(upload_to=order_scan_upload_to, verbose_name='Скан приказа', blank=True, null=True)
+    scan = models.FileField(
+        upload_to=order_scan_upload_to,
+        verbose_name='Скан приказа',
+        blank=True,
+        null=True)
 
     class Meta:
         verbose_name = 'Приказ'
@@ -47,4 +71,3 @@ class Order(models.Model):
 
     def __str__(self):
         return f'{self.document_number}'
-

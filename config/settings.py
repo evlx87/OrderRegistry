cd389aedd8689da56e150202676163e65c648654
evlx <evilnyagaming@yandex.ru>
2024-12-23 09:34:41 +0300

    исправлена ошибка переадресации после авторизации пользователя

--- config/settings.py
+++ config/settings.py
@@ -144,4 +144,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = 'order:index'
+LOGIN_REDIRECT_URL = 'orders:index'
